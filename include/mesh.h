5641825756704630fafe1de9d98896df306166f1
fra-zz-mer <aaa@bbb.com>
2020-03-29 22:23:12 +0700

    wip

--- include/mesh.h
+++ include/mesh.h
@@ -2,19 +2,28 @@
 #include "common.h"
 #include "icorerender.h"
 
+
+struct MeshData
+{
+	std::vector<vec3> triangles;
+};
+
 class Mesh
 {
-	std::unique_ptr<ICoreMesh> coreMesh_;
+	std::unique_ptr<ICoreMesh> coreMeshPtr;
 	std::string path_;
 	vec3 center_;
+	std::shared_ptr<MeshData> trianglesDataPtr;
 
 public:
 	Mesh(const std::string& path);
 	~Mesh();
 
 	bool Load();
+	std::shared_ptr<MeshData> GetTrianglesData();
+	bool isSphere();
 
-	auto DLLEXPORT GetCoreMesh() -> ICoreMesh* { return coreMesh_.get(); }
+	auto DLLEXPORT GetCoreMesh() -> ICoreMesh* { return coreMeshPtr.get(); }
 	auto DLLEXPORT GetAttributes() -> INPUT_ATTRUBUTE;
 	auto DLLEXPORT GetVideoMemoryUsage() -> size_t;
 	auto DLLEXPORT GetPath() -> const char* const { return path_.c_str(); }
